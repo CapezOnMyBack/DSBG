9219bb321c774875f2bcf0d651fd40a6bc276ebc
CapezOnMyBack <aleksandar03@protonmail.com>
2025-09-05 15:33:21 +0200

    init 1.21.1

--- src/main/java/ch/capez/dsbg/Dsbg.java
+++ src/main/java/ch/capez/dsbg/Dsbg.java
@@ -1,43 +1,58 @@
 package ch.capez.dsbg;
 
-import ch.capez.dsbg.config.DsbgServerConfig;
 import com.mojang.logging.LogUtils;
+import net.minecraft.client.Minecraft;
+import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
-import net.minecraftforge.fml.event.config.ModConfigEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
-@Mod(Dsbg.MODID)
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(Dsbg.MOD_ID)
 public class Dsbg {
+    // Define mod id in a common place for everything to reference
+    public static final String MOD_ID = "dsbg";
+    // Directly reference a slf4j logger
+    public static final Logger LOGGER = LogUtils.getLogger();
 
-    public static final String MODID = "dsbg";
-    private static final Logger LOGGER = LogUtils.getLogger();
+    public Dsbg(FMLJavaModLoadingContext context) {
+        IEventBus modEventBus = context.getModEventBus();
 
-    public Dsbg() {
-        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
-        ModLoadingContext.get().registerConfig(ModConfig.Type.SERVER, DsbgServerConfig.SPEC, "dsbg-server.toml");
+        // Register the commonSetup method for modloading
+        modEventBus.addListener(this::commonSetup);
 
-        modEventBus.addListener(this::onCommonSetup);
-        modEventBus.addListener(this::onConfigReload);
-
+        // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
+
+        // Register our mod's ForgeConfigSpec so that Forge can create and load the config file for us
+        context.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
     }
 
-    public void onCommonSetup(FMLCommonSetupEvent event) {
-        LOGGER.info("Loading DSBG...");
+    private void commonSetup(final FMLCommonSetupEvent event) {
     }
 
-    public void onConfigReload(ModConfigEvent.Reloading event) {
-        LOGGER.info("DSBG Mod loaded: " + printInitMessage(DsbgServerConfig.SUPPRESS_LOGGING.get()));
+    // You can use SubscribeEvent and let the Event Bus discover methods to call
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {
+        // Do something when the server starts
+        LOGGER.info("HELLO from server starting");
     }
 
-    private String printInitMessage(Boolean state) {
-        return state ? "SUPPRESSING setBlock error logs!" : "PRINTING setBlock error logs!";
+    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+            // Some client setup code
+            LOGGER.info("HELLO FROM CLIENT SETUP");
+            LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
+        }
     }
-
 }
